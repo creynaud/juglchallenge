70c0246738a68567e67c510b5091bc1849e52dde
Claire Reynaud <claire12.reynaud@laposte.net>
2013-02-09 21:00:19 +0100

    Ask again for the markdown file

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,9 +8,7 @@ object Application extends Controller {
 
   def email(q: Option[String]) = Action {
     q match {
-      case Some("Quelle est ton adresse email") => Ok("claire12.reynaud@laposte.net")
-      case Some("Es tu abonne a la mailing list(OUI/NON)") => Ok("OUI")
-      case Some("Es tu heureux de participer(OUI/NON)") => Ok("OUI")
+      case Some("As tu bien recu le premier enonce") => Ok("NON")
       case Some("Es tu pret a recevoir une enonce au format markdown par http post(OUI/NON)") => Ok("OUI")
       case _ => Ok("C'est pas faux")
     }
