4df49c536c8a81700aec14c096010748cc0c0880
Claire Reynaud <claire12.reynaud@laposte.net>
2013-02-09 20:35:32 +0100

    Handle the 4th and 5th questions

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,6 +1,8 @@
 package controllers
 
 import play.api.mvc._
+import java.io._
+import io.Source
 
 object Application extends Controller {
 
@@ -9,8 +11,24 @@ object Application extends Controller {
       case Some("Quelle est ton adresse email") => Ok("claire12.reynaud@laposte.net")
       case Some("Es tu abonne a la mailing list(OUI/NON)") => Ok("OUI")
       case Some("Es tu heureux de participer(OUI/NON)") => Ok("OUI")
+      case Some("Es tu pret a recevoir une enonce au format markdown par http post(OUI/NON)") => Ok("OUI")
       case _ => Ok("C'est pas faux")
     }
+  }
+
+  def markdown = Action(parse.temporaryFile) {
+    request =>
+      val file: File = new File("/tmp/markdown")
+      file.delete()
+      request.body.moveTo(file)
+      Ok("File uploaded")
+  }
+
+  def getMarkdown = Action {
+    val source = Source.fromFile("/tmp/markdown")
+    val lines = source.mkString
+    source.close()
+    Ok(lines)
   }
 
 }
