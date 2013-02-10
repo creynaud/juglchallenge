6a402cf883ad2a3b75a2e6eac62a13751fc1e233
Claire Reynaud <claire12.reynaud@laposte.net>
2013-02-10 20:09:58 +0100

    Add an assert to check the sum of the solution is 0

--- app/model/DietSolver.scala
+++ app/model/DietSolver.scala
@@ -19,9 +19,12 @@ class DietSolver(activities: List[Activity]) {
     solve(sumAllNegatives, sumAllPositives, activities.length - 1, 0, solution)
     val solutionIndexesList: Option[ListBuffer[Int]] = solution.get(activities.length - 1, 0)
     val result: mutable.Set[Activity] = mutable.Set[Activity]()
+    var checksum: Int = 0
     for (index <- solutionIndexesList.get) {
       result += activities(index)
+      checksum += activities(index).value
     }
+    assert (checksum == 0)
     return result.toSet
   }
 
@@ -37,15 +40,18 @@ class DietSolver(activities: List[Activity]) {
 
   // http://en.wikipedia.org/wiki/Subset_sum_problem#Pseudo-polynomial_time_dynamic_programming_solution
   def solve(sumAllNegatives: Int, sumAllPositives: Int, index: Int, sum: Int, solution: mutable.Map[(Int, Int), ListBuffer[Int]]) {
-    solution.put((index, sum), ListBuffer[Int]())
     if (sum < sumAllNegatives) {
+      solution.put((index, sum), ListBuffer[Int]())
       return
     } else if (sum > sumAllPositives) {
+      solution.put((index, sum), ListBuffer[Int]())
       return
     }
     if (index == 0) {
       if (sum == activities(0).value) {
-        solution.put((index, sum), ListBuffer[Int](0))
+        solution.put((0, sum), ListBuffer[Int](0))
+      } else {
+        solution.put((0, sum), ListBuffer[Int]())
       }
     } else {
       val existingSolutionIndexMinus1Sum: Option[ListBuffer[Int]] = solution.get((index - 1, sum))
@@ -70,6 +76,12 @@ class DietSolver(activities: List[Activity]) {
       if (solutionIndexMinus1SumMinusValue.length > 0) {
         solutionIndexMinus1SumMinusValue += index
         solution.put((index, sum), solutionIndexMinus1SumMinusValue)
+      }
+
+      val existingSolutionIndexSum: Option[ListBuffer[Int]] = solution.get((index, sum))
+      existingSolutionIndexSum match {
+        case None => solution.put((index, sum), ListBuffer[Int]())
+        case _ =>
       }
     }
   }
