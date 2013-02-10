ca0d442e400d29a14f93642499209ad6d702d36d
Claire Reynaud <claire12.reynaud@laposte.net>
2013-02-10 11:49:24 +0100

    Answer next question

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -6,16 +6,14 @@ import play.api.test._
 import play.api.test.Helpers._
 
 /**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- * For more information, consult the wiki.
+ * Tests for the application.
  */
 class ApplicationSpec extends Specification {
 
   "Application" should {
-    "return OUI for /?q=As+tu+trouve+le+dernier+exercice+difficile(OUI/NON)" in {
+    "return OUI for /?q=As+tu+bien+recu+le+deuxieme+enonce(OUI/NON)" in {
       running(FakeApplication()) {
-        val home = route(FakeRequest(GET, "/?q=As+tu+trouve+le+dernier+exercice+difficile(OUI/NON)")).get
+        val home = route(FakeRequest(GET, "/?q=As+tu+bien+recu+le+deuxieme+enonce(OUI/NON)")).get
 
         status(home) must equalTo(OK)
         contentType(home) must beSome.which(_ == "text/plain")
--- test/MineSweeperSolverSpec.scala
+++ test/MineSweeperSolverSpec.scala
@@ -2,8 +2,6 @@ package test
 
 import org.specs2.mutable._
 
-import play.api.test._
-import play.api.test.Helpers._
 import controllers._
 
 /**
