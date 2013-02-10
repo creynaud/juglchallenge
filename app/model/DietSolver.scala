cc7d9cc4bc5371eb170f906d301b86d66270029c
Claire Reynaud <claire12.reynaud@laposte.net>
2013-02-10 18:04:25 +0100

    Simplified the subset sum problem algo: use only one map to store solutions

--- app/model/DietSolver.scala
+++ app/model/DietSolver.scala
@@ -6,9 +6,8 @@ import collection.mutable.ListBuffer
 class DietSolver(activities: List[Activity]) {
 
   def solution(): Set[Activity] = {
-    val canBeSolved: mutable.Map[(Int, Int), Boolean] = mutable.Map[(Int, Int), Boolean]()
     val solution: mutable.Map[(Int, Int), ListBuffer[Int]] = mutable.Map[(Int, Int), ListBuffer[Int]]()
-    solve(activities.length -1, 0, canBeSolved, solution)
+    solve(activities.length -1, 0, solution)
     val solutionIndexesList: Option[ListBuffer[Int]] = solution.get(activities.length - 1, 0)
     solutionIndexesList match {
       case Some(_) => {
@@ -33,34 +32,34 @@ class DietSolver(activities: List[Activity]) {
   }
 
   // http://en.wikipedia.org/wiki/Subset_sum_problem#Pseudo-polynomial_time_dynamic_programming_solution
-  def solve(index: Int, sum: Int, canBeSolved: mutable.Map[(Int, Int), Boolean], solution: mutable.Map[(Int, Int), ListBuffer[Int]]) {
+  def solve(index: Int, sum: Int, solution: mutable.Map[(Int, Int), ListBuffer[Int]]) {
     if (index == 0) {
-      val canMakeSum: Boolean = sum == activities(0).value
-      canBeSolved.put((0, sum), canMakeSum)
-      if (canMakeSum) {
+      if (sum == activities(0).value) {
         solution.put((0, sum), ListBuffer[Int](0))
       }
     } else {
-      solve(index - 1, sum, canBeSolved, solution)
-      solve(index - 1, sum - activities(index).value, canBeSolved, solution)
-      var canMakeSum: Boolean = canBeSolved.get((index - 1, sum)).get
-      if (canMakeSum) {
-        val solutionForIndexMinus1: ListBuffer[Int] = solution.get((index - 1, sum)).get
-        solution.put((index, sum), solutionForIndexMinus1)
+      solve(index - 1, sum, solution)
+      solve(index - 1, sum - activities(index).value, solution)
+      val solutionOption: Option[ListBuffer[Int]] = solution.get((index - 1, sum))
+      solutionOption match {
+        case Some(_) => {
+          val solutionForIndexMinus1: ListBuffer[Int] = solution.get((index - 1, sum)).get
+          solution.put((index, sum), solutionForIndexMinus1)
+        }
+        case _ =>
       }
-      val currentValueIsSum: Boolean = activities(index).value == sum
-      canMakeSum |= currentValueIsSum
-      if (currentValueIsSum) {
+      if (activities(index).value == sum) {
         solution.put((index, sum), ListBuffer[Int](index))
       }
-      val canMakeSumMinusCurrentValue: Boolean = canBeSolved.get((index - 1, sum - activities(index).value)).get
-      canMakeSum |= canMakeSumMinusCurrentValue
-      if (canMakeSumMinusCurrentValue) {
-        val solutionForIndexMinus1: ListBuffer[Int] = solution.get((index - 1, sum - activities(index).value)).get
-        solutionForIndexMinus1 += index
-        solution.put((index, sum), solutionForIndexMinus1)
+      val otherSolutionOption: Option[ListBuffer[Int]] = solution.get((index - 1, sum - activities(index).value))
+      otherSolutionOption match {
+        case Some(_) => {
+          val solutionForIndexMinus1: ListBuffer[Int] = solution.get((index - 1, sum - activities(index).value)).get
+          solutionForIndexMinus1 += index
+          solution.put((index, sum), solutionForIndexMinus1)
+        }
+        case _ =>
       }
-      canBeSolved.put((index, sum), canMakeSum)
     }
   }
 }
--- test/DietSolverSpec.scala
+++ test/DietSolverSpec.scala
@@ -26,21 +26,21 @@ class DietSolverSpec extends Specification {
     "Find a solution" in {
       val activitiesListBuffer = mutable.ListBuffer[Activity]()
       activitiesListBuffer += new Activity("coca-light", 1)
-      activitiesListBuffer += new Activity("ga", 1)
+      val ga = new Activity("ga", 1)
+      activitiesListBuffer += ga
       activitiesListBuffer += new Activity("bu", 42)
       activitiesListBuffer += new Activity("zo", 3)
       activitiesListBuffer += new Activity("meuh", -112)
-      activitiesListBuffer += new Activity("croissant", 180)
+      val croissant = new Activity("croissant", 180)
+      activitiesListBuffer += croissant
       activitiesListBuffer += new Activity("kinder", 212)
       activitiesListBuffer += new Activity("burger", 612)
       activitiesListBuffer += new Activity("au-travail-a-velo", -113)
       activitiesListBuffer += new Activity("standing-desk", -400)
-      activitiesListBuffer += new Activity("guitar-hero", -181)
+      val guitarHero = new Activity("guitar-hero", -181)
+      activitiesListBuffer += guitarHero
       val solver: DietSolver = new DietSolver(activitiesListBuffer.toList)
-      val canBeSolved: mutable.Map[(Int, Int), Boolean] = mutable.Map[(Int, Int), Boolean]()
-      val solution: mutable.Map[(Int, Int), mutable.ListBuffer[Int]] = mutable.Map[(Int, Int), mutable.ListBuffer[Int]]()
-      solver.solve(activitiesListBuffer.length -1, 0, canBeSolved, solution)
-      canBeSolved.get((activitiesListBuffer.length -1, 0)).get mustEqual(true)
+      solver.solution() must equalTo(Set(ga, croissant, guitarHero))
     }
     "Find no solution" in {
       val activitiesListBuffer = mutable.ListBuffer[Activity]()
@@ -56,10 +56,6 @@ class DietSolverSpec extends Specification {
       activitiesListBuffer += new Activity("standing-desk", 400)
       activitiesListBuffer += new Activity("guitar-hero", 181)
       val solver: DietSolver = new DietSolver(activitiesListBuffer.toList)
-      val canBeSolved: mutable.Map[(Int, Int), Boolean] = mutable.Map[(Int, Int), Boolean]()
-      val solution: mutable.Map[(Int, Int), mutable.ListBuffer[Int]] = mutable.Map[(Int, Int), mutable.ListBuffer[Int]]()
-      solver.solve(activitiesListBuffer.length -1, 0, canBeSolved, solution)
-      canBeSolved.get((activitiesListBuffer.length -1, 0)).get mustEqual(false)
       solver.solution() must equalTo(Set())
     }
   }
